3818bc9a5f5f7f001f4deb8bd115c510b3912af6
achrams <ramadhanovic@gmail.com>
2020-03-30 19:15:08 +0700

    add update and delete

--- controllers/TodoController.js
+++ controllers/TodoController.js
@@ -43,7 +43,7 @@ class TodoController {
         Todo.create(newdata)
             .then(data => {
                 res.status(201).json({ newtododata: data })
-                    // else res.status(400).json({ "type": "Error", "msg": "Validation Error", newtododata })
+
             })
             .catch(err => {
                 res.status(500).json({
@@ -51,6 +51,62 @@ class TodoController {
                     "msg": "Internal Server Error"
                 })
             })
+    }
+
+    static update(req, res) {
+        let id = +req.params.id
+        let { title, description, status, due_date } = req.body
+        let updatedData = {
+            title,
+            description,
+            status,
+            due_date
+        }
+
+        Todo.findOne({ where: { id: id } })
+            .then(result => {
+                if (!result) res.status(404).json({
+                    "type": "Error",
+                    "msg": "Data not found"
+                })
+                else {
+                    Todo.update(updatedData, { where: { id: result.id } })
+                        .then(updated => {
+                            res.status(200).json({ updatedData })
+                        })
+                }
+            })
+            .catch(err => {
+                res.status(500).json({
+                    "type": "Error",
+                    "msg": "Internal Server Error"
+                })
+            })
+    }
+
+    static delete(req, res) {
+        let id = +req.params.id
+
+        Todo.findOne({ where: { id: id } })
+            .then(data => {
+                if (!data) res.status(404).json({
+                    "type": "Error",
+                    "msg": "Data not found"
+                })
+                else {
+                    Todo.destroy({ where: { id: id } })
+                        .then(deleted => {
+                            res.status(200).json({ data, deleted })
+                        })
+                }
+            })
+            .catch(err => {
+                res.status(500).json({
+                    "type": "Error",
+                    "msg": "Internal Server Error"
+                })
+            })
+
     }
 }
 
--- routes/todo.js
+++ routes/todo.js
@@ -4,4 +4,7 @@ const TodoController = require('../controllers/TodoController')
 router.get('/', TodoController.findAll)
 router.post('/', TodoController.create)
 router.get('/:id', TodoController.findOne)
+router.put('/:id', TodoController.update)
+router.delete('/:id', TodoController.delete)
+
 module.exports = router
